35276a776dd0bcb37e61a5097e9d30016159a5cd
@vasu <>
2023-03-24 19:11:30 +0530

    compressordecompressor project

---
+++ src/main/java/compressor_decompressor/compressor.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compressor_decompressor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+/**
+ *
+ * @author USER
+ */
+public class compressor {
+      public static void method(File file) throws IOException{
+        String fileDirectory= file.getParent();
+        FileInputStream fis=new FileInputStream(file);
+        FileOutputStream fos=new FileOutputStream(fileDirectory+"/Compressedfile.gz");
+        GZIPOutputStream gzipOS=new GZIPOutputStream(fos);
+        
+        byte[] buffer =new byte[1024];
+        int len;
+        
+        while((len=fis.read(buffer)) != -1){
+            gzipOS.write(buffer,0,len);
+        }
+        gzipOS.close();
+        fos.close();
+        fis.close();
+        
+    }
+    public static void main(String[] args) throws IOException{
+        File path=new File("/Users/shivenduac/Downloads/Compressor/compressordecompressor/Testfile");
+        method(path);
+    }
+    
+}
+    
+
